a7c6a220760e588433342d00fc42692bb3830663
RarCeth <rarceth@gmail.com>
2017-01-19 08:22:09 +0800

    well shit

--- Assets/Controller_Handler.cs
+++ Assets/Controller_Handler.cs
@@ -10,19 +10,20 @@ public class Controller_Handler : MonoBehaviour {
 
 	public SteamVR_Controller.Device device;
 
+	SteamVR_TrackedController LH, RH;
 	// Use this for initialization
 	void OnEnable () {
 
 		if (player != null) {
 			if (leftHand) {
-				SteamVR_TrackedController LH = GetComponent<SteamVR_TrackedController> ();
+				LH = GetComponent<SteamVR_TrackedController> ();
 				player.leftHand = LH;
 				device = SteamVR_Controller.Input ((int)player.leftHand.controllerIndex);
 				player.leftDevice = device;
 				player.leftHandC = GetComponent<Controller_Handler> ();
 				LH.Gripped += player.PickUpWithLeft;
 			} else if (rightHand) {
-				SteamVR_TrackedController RH = GetComponent<SteamVR_TrackedController> ();
+				RH = GetComponent<SteamVR_TrackedController> ();
 				player.rightHand = RH;
 				device = SteamVR_Controller.Input ((int)player.rightHand.controllerIndex);
 				player.rightDevice = device;
@@ -32,14 +33,14 @@ public class Controller_Handler : MonoBehaviour {
 		} else {
 			player = FindObjectOfType<Player_Main> ();
 			if (leftHand) {
-				SteamVR_TrackedController LH = GetComponent<SteamVR_TrackedController> ();
+				LH = GetComponent<SteamVR_TrackedController> ();
 				player.leftHand = LH;
 				device = SteamVR_Controller.Input ((int)player.leftHand.controllerIndex);
 				player.leftDevice = device;
 				player.leftHandC = GetComponent<Controller_Handler> ();
 				LH.Gripped += player.PickUpWithLeft;
 			} else if (rightHand) {
-				SteamVR_TrackedController RH = GetComponent<SteamVR_TrackedController> ();
+				RH = GetComponent<SteamVR_TrackedController> ();
 				player.rightHand = RH;
 				device = SteamVR_Controller.Input ((int)player.rightHand.controllerIndex);
 				player.rightDevice = device;
@@ -53,6 +54,19 @@ public class Controller_Handler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void OnDisable () {
-		
+		if (player != null) {
+			if (leftHand) {
+				LH.Gripped -= player.PickUpWithLeft;
+			} else if (rightHand) {
+				RH.Gripped += player.PickUpWithRight;
+			}
+		} else {
+			player = FindObjectOfType<Player_Main> ();
+			if (leftHand) {
+				LH.Gripped += player.PickUpWithLeft;
+			} else if (rightHand) {
+				RH.Gripped += player.PickUpWithRight;
+			}
+		}
 	}
 }
