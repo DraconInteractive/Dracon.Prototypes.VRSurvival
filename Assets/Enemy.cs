f78ca97b58e065d55fd3d200add472b5f369647d
RarCeth <rarceth@gmail.com>
2017-01-27 10:37:53 +0800

    Enemy+

---
+++ Assets/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	List<GameObject> targets = new List<GameObject>();
+    public List<GameObject> detectedTargets = new List<GameObject>();
+	public GameObject eye;
+
+	public int health, maxHealth;
+	// Use this for initialization
+	void Start () {
+		targets.Add (Player_Main.player.gameObject);
+		health = maxHealth;
+	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void Detection () {
+		for (int i = 0; i < targets.Count; i++) {
+			if (Quaternion.Angle (transform.rotation, Quaternion.LookRotation (targets [i].transform.position - transform.position)) < 90) {
+				RaycastHit hit;
+				Ray ray = new Ray (eye.transform.position, eye.transform.forward);
+
+				if(Physics.Raycast(ray, out hit, 100)) {
+					GameObject hObj = hit.collider.gameObject;
+					if (hObj == targets[i]) {
+						if (!detectedTargets.Contains(hObj)) {
+							detectedTargets.Add (hObj);
+						}
+					}
+				} else {
+					if (detectedTargets.Contains(hit.collider.gameObject)) {
+						detectedTargets.Remove (hit.collider.gameObject);
+					}
+				}
+			} else {
+				if (detectedTargets.Contains(targets[i])) {
+					detectedTargets.Remove (targets[i]);
+				}
+			}
+				
+		}
+	}
+
+	public void Damage (int damage) {
+		health -= damage;
+		if (health <= 0) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public static MainMenu menu;
+	Player_Main player;
+	public Button invButton, playerMenuButton, helpButton, exitButton;
+
+	public int yOffset;
+	void Awake () {
+		if (menu != null) {
+			Destroy (menu.gameObject);
+		}
+		menu = GetComponent<MainMenu> ();
+		exitButton.onClick.AddListener (() => QuitGame ());
+	}
+	// Use this for initialization
+	void Start () {
+		player = Player_Main.player;
+
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, 100)) {
+			if (hit.collider.gameObject.tag == "Ground") {
+				transform.position = hit.point + Vector3.up * yOffset;
+				Quaternion.LookRotation ((player.transform.position - transform.position), Vector3.up);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+
+
+	void QuitGame () {
+		Application.Quit ();
+	}
+}
