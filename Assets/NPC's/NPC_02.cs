6c7912234dbf769e24e0d587ac27ceb688e3e771
RarCeth <rarceth@gmail.com>
2017-02-24 15:58:22 +0800

    uytr

--- Assets/NPC's/NPC_02.cs
+++ Assets/NPC's/NPC_02.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class NPC_02 : NPC {
 
+	public GameObject npcUI;
+
 	public override void BeginInteraction () {
 		base.BeginInteraction ();
 		GetComponent<BoxCollider> ().enabled = true;
+		npcUI.SetActive (true);
 	}
 
 	public override void EndInteraction () {
 		base.EndInteraction ();
 		GetComponent<BoxCollider> ().enabled = false;
-		
+		npcUI.SetActive (false);
 	}
 }
