7f1858e7edce536cbafe67710fb15c17b3dce613
RarCeth <rarceth@gmail.com>
2017-03-09 19:49:59 +0800

    sdg

--- Assets/Items/SciFi_Bow_Notch.cs
+++ Assets/Items/SciFi_Bow_Notch.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class SciFi_Bow_Notch : Base_Item {
 
-	public GameObject arrow;
-	internal override void Update () {}
+	public GameObject arrowTemplate;
+	public GameObject initNotchPos;
+	public float maxStretch;
+
+	GameObject nockedArrow;
+	internal override void Update () {
+//		1. convert the position to local-space
+//		2. set the z to the value(s) you want
+//		3. convert back in-to work space.
+
+		if (controllerObj != null)
+		{
+			Vector3 localTargetPos = controllerObj.transform.TransformPoint (controllerObj.transform.position);
+//			transform.position = controllerObj.transform.position;
+//			transform.rotation = controllerObj.transform.rotation;
+		}
+		if (Vector3.Distance(initNotchPos.transform.position, transform.position) > maxStretch) {
+			transform.position = initNotchPos.transform.position + (transform.position - initNotchPos.transform.position).normalized * maxStretch;
+		}
+	}
 
 	public override void OnPickup (GameObject hand, HTC.UnityPlugin.Vive.HandRole handRole)
 	{
@@ -19,9 +37,15 @@ public class SciFi_Bow_Notch : Base_Item {
 
 	void OnGrabbed () {
 		//Instantiate Arrow (instantiate + parent)
+		nockedArrow = Instantiate (arrowTemplate, transform.position, Quaternion.identity, transform) as GameObject;
 	}
 
 	void OnRelease () {
 		//Fire Arrow (unparent + addForce)
+		nockedArrow.transform.parent = null;
+
+		nockedArrow.GetComponent<Rigidbody> ().AddForce ((initNotchPos.transform.position - transform.position).normalized * 10, ForceMode.VelocityChange);
+	
+		transform.position = initNotchPos.transform.position;
 	}
 }
